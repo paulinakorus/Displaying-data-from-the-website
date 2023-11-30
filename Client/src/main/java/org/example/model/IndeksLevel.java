dd3320f20f0230b2b159bc4e3d0531b845bf7ab7
Paulina Korus <paukor2606@gmail.com>
2023-11-30 17:01:34 +0100

    Making IndeksLevel class

---
+++ Client/src/main/java/org/example/model/IndeksLevel.java
@@ -0,0 +1,27 @@
+package org.example.model;
+
+public class IndeksLevel {
+    private int id;
+    private String indexLevelName;
+
+    public IndeksLevel(int id, String indexLevelName){
+        this.id = id;
+        this.indexLevelName = indexLevelName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIndexLevelName() {
+        return indexLevelName;
+    }
+
+    public void setIndexLevelName(String indexLevelName) {
+        this.indexLevelName = indexLevelName;
+    }
+}
