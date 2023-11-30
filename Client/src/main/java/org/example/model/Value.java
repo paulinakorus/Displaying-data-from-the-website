e2436ef38ac16b68f72627d0da76bb3a07cef335
Paulina Korus <paukor2606@gmail.com>
2023-11-30 16:50:04 +0100

    Making Value class

---
+++ Client/src/main/java/org/example/model/Value.java
@@ -0,0 +1,27 @@
+package org.example.model;
+
+public class Value {
+    private String date;
+    private double value;
+
+    public Value(String date, double value) {
+        this.date = date;
+        this.value = value;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+}
