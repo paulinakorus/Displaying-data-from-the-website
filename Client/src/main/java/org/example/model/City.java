5ee46ab9928bd7eeacf48256c9e2f77b60740af8
Paulina Korus <paukor2606@gmail.com>
2023-11-30 16:23:35 +0100

    Making City class

---
+++ Client/src/main/java/org/example/model/City.java
@@ -0,0 +1,37 @@
+package org.example.model;
+
+public class City {
+    private int id;
+    private String name;
+    private Commune commune;
+
+    public City (int id, String name, Commune commune){
+        this.id = id;
+        this.name = name;
+        this.commune = commune;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Commune getCommune() {
+        return commune;
+    }
+
+    public void setCommune(Commune commune) {
+        this.commune = commune;
+    }
+}
