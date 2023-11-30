4d243d999c61f3764d0aea599029d842d5b2376c
Paulina Korus <paukor2606@gmail.com>
2023-11-30 17:05:58 +0100

    Making IndeksInfo class

---
+++ Client/src/main/java/org/example/model/IndeksInfo.java
@@ -0,0 +1,37 @@
+package org.example.model;
+
+public class IndeksInfo {
+    private String calcDate;
+    private IndeksLevel indeksLevel;
+    private String sourceDataDate;
+
+    public IndeksInfo(String calcDate, IndeksLevel indeksLevel, String sourceDataDate){
+        this.indeksLevel = indeksLevel;
+        this.calcDate = calcDate;
+        this.sourceDataDate = sourceDataDate;
+    }
+
+    public String getCalcDate() {
+        return calcDate;
+    }
+
+    public void setCalcDate(String calcDate) {
+        this.calcDate = calcDate;
+    }
+
+    public IndeksLevel getIndeksLevel() {
+        return indeksLevel;
+    }
+
+    public void setIndeksLevel(IndeksLevel indeksLevel) {
+        this.indeksLevel = indeksLevel;
+    }
+
+    public String getSourceDataDate() {
+        return sourceDataDate;
+    }
+
+    public void setSourceDataDate(String sourceDataDate) {
+        this.sourceDataDate = sourceDataDate;
+    }
+}
