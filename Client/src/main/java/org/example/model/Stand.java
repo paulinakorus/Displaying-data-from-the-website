d08b8b7e5c3e9350a6b276486e2d1d946fb10ed0
Paulina Korus <paukor2606@gmail.com>
2023-11-30 16:39:29 +0100

    Making Stand class

---
+++ Client/src/main/java/org/example/model/Stand.java
@@ -0,0 +1,37 @@
+package org.example.model;
+
+public class Stand {
+    private int standId;
+    private int stationId;
+    private Param param;
+
+    public Stand(int standId, int stationId, Param param) {
+        this.standId = standId;
+        this.stationId = stationId;
+        this.param = param;
+    }
+
+    public int getStandId() {
+        return standId;
+    }
+
+    public void setStandId(int standId) {
+        this.standId = standId;
+    }
+
+    public int getStationId() {
+        return stationId;
+    }
+
+    public void setStationId(int stationId) {
+        this.stationId = stationId;
+    }
+
+    public Param getParam() {
+        return param;
+    }
+
+    public void setParam(Param param) {
+        this.param = param;
+    }
+}
