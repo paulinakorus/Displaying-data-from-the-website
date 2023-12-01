d6156662ccbf7e316f98664ce3fdd825e407c726
Paulina Korus <paukor2606@gmail.com>
2023-12-01 20:39:45 +0100

    Changing JSON to Jackson

--- Client/src/main/java/module-info.java
+++ Client/src/main/java/module-info.java
@@ -1,4 +1,9 @@
 module Client {
     requires org.json;
     requires java.net.http;
+    requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.databind;
+
+    opens org.example.model to com.fasterxml.jackson.databind;
+    exports org.example.model;
 }
